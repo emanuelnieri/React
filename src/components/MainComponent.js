8b8bc215b12fe56209a67806d078f20bb5c1e8c3
Emanuel Nieri <emanuel.nieri@gmail.com>
2022-02-22 17:29:28 -0300

    Week2 Commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
+import { DISHES } from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        dishes: DISHES,
+        selectedDish: null
+    };
+  }
+
+  onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId});
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        {/* COMENTÁRIO JSX */}
+        {/* MenuComponent renderiza dishes, e ao clicar armazena onDishSelect */}
+        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        {/* DishDetail renderiza os detalhes do onDishSelect */}
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Main;
